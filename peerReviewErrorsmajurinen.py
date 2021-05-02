5af419900863f5f42fbb520f71d64d8c83b08d69
brianmajurinen <83433864+brianmajurinen@users.noreply.github.com>
2021-05-02 17:52:00 -0500

    Add files via upload

---
+++ peerReviewErrorsmajurinen.py
@@ -0,0 +1,61 @@
+# This is a header for the application
+# You should read this header and insert your name and your date below as part of the peer review
+# This is a typical part of any program
+# Author: <Brian Majurinen>
+# Creation Date: <May 1, 2021>
+# Below is a simple program with 10 issues (some are syntax errors and some are logic errors.  You need to identify the issues and correct them.
+
+import random
+import time
+
+def displayIntro():
+	print('''You are in a land full of dragons. In front of you,
+	you see two caves. In one cave, the dragon is friendly
+	and will share his treasure with you. The other dragon
+	is greedy and hungry, and will eat you on sight.''')
+	print()
+#indentation errors
+def chooseCave():
+	cave = ''
+	while cave != "1" and cave != "2":
+	    print('Which cave will you go into? (1 or 2)')
+	    cave = input()
+        #was returning caves when should return cave
+	return cave
+
+def checkCave(chosenCave):
+	print('You approach the cave...')
+	#sleep for 2 seconds
+	time.sleep(2)
+	print('It is dark and spooky...')
+	#sleep for 2 seconds
+	time.sleep(3)
+	print('A large dragon jumps out in front of you! He opens his jaws and...')
+	print()
+	#sleep for 2 seconds
+	time.sleep(2)
+	friendlyCave = random.randint(1, 2)
+
+	if chosenCave == str(friendlyCave):
+		print('Gives you his treasure!')
+	else:
+        #missing () in print statement below
+		print ('Gobbles you down in one bite!')
+
+def restart():
+	print('Do you want to play again? (yes or no)')
+	playAgain = input()
+	while playAgain == "yes" or playAgain == "y":
+		displayIntro()
+    	#improper camelCasing below
+		caveNumber = chooseCave()
+		checkCave(caveNumber)
+	else:
+    #spelling error
+		print("Thanks for playing")
+		break
+
+#moved modules
+displayIntro()
+caveNumber = chooseCave()
+restart()
